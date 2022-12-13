ebfa777af433ee0841f200f4f135d02c13518c0d
bishal-github <shreesbishal44@gmail.com>
2022-12-13 18:20:04 +0545

    adding onclick enent and using menuOpen and setMenuOpen

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-// import {PersonIcon} from '@material-ui/core';
 import {Person, Mail} from '@material-ui/icons';
-import './topbar.scss'
-export default function Topbar() {
+import './topbar.scss';
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='topbar'>
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <a href="#intro" className='logo'>bishal</a>
@@ -18,7 +17,7 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className='line1'></span>
             <span className='line2'></span>
             <span className='line3'></span>
