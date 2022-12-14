c997894f2b78d992f0227cbe50a108ae0aed7ae8
bishal-github <shreesbishal44@gmail.com>
2022-12-14 16:06:13 +0545

    listing items and adding images inside the container div

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,41 @@ import './portfolio.scss'
 
 export default function Portfolio() {
   return (
-    <div className='portfolio' id='portfolio'>Portfolio</div>
-  )
+    <div className='portfolio' id='portfolio'>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Designing</li>
+        <li>Branding</li>
+      </ul>
+       <div className="container">
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+          <div className="item">
+            <img src="https://www.ideamotive.co/hubfs/app%20development%20cost.png" alt="" />
+            <h3>Banking App</h3>
+          </div>
+        </div>
+    </div>
+  )  
 }
