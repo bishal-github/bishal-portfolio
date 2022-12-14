426fb990783f2a6f562770fe0c0fc5509afb16c2
bishal-github <shreesbishal44@gmail.com>
2022-12-14 08:31:02 +0545

    adding pic , text animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import Typical from 'react-typical'
 import './intro.scss'
 
 export default function Intro() {
+  
   return (
     <div className='intro' id='intro'>
-      intro
-    </div>
+      <div className="left">
+<div className="imgContainer">
+  <img src="assests/man.jpg" alt="" />
+</div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Bishal Shrees</h1>
+          <h3><Typical
+        steps={['React Developer', 1000, 'Fronted Developer', 1000, 'Web Developer',1000 ]}
+        loop={Infinity}
+        wrapper="b"
+      />
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assests/down.png" alt="" />
+        </a>
+
+      </div>
+    </div> 
   )
 }
