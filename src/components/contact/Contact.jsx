6540c40709fcb90e298471d077b18bdad876d1d7
bishal-github <shreesbishal44@gmail.com>
2022-12-19 18:56:03 +0545

    added image, input, text area button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 import './contact.scss'
 
 export default function Contact() {
+const handleSubmit =(e)=>{
+  e.preventDefault();
+}
+
   return (
-    <div className='contact' id='contact'>Contact</div>
-  )
+    <div className='contact' id='contact'>
+      <div className="left">
+        <img src="assests/shake.svg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value="" placeholder='Email' />
+          <textarea placeholder='Message'></textarea>
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    </div>
+  ) 
 }
