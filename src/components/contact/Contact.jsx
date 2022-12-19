b5bb7a571dfb9636356ddc7d8ca02276d337628b
bishal-github <shreesbishal44@gmail.com>
2022-12-19 19:09:21 +0545

    adding usestate hook to show message after clicking send button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useState } from 'react';
 import './contact.scss'
 
 export default function Contact() {
+const [message, setMessage] = useState(false);
+
 const handleSubmit =(e)=>{
   e.preventDefault();
+  setMessage(true);
 }
 
   return (
@@ -17,6 +21,7 @@ const handleSubmit =(e)=>{
           <input type="text" value="" placeholder='Email' />
           <textarea placeholder='Message'></textarea>
           <button type='submit'>Send</button>
+          {message && <span>Thanks, I'will reply ASAP :)</span>}
         </form>
       </div>
     </div>
